4ae7e1df0695f0f68ec5f7d54dff88e0910b5932
Reinier Blanco <reinierblanco10@gmail.com>
2019-05-06 04:56:48 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,51 @@
+//basic math using functions
+var a, b;
+
+function add(a, b){
+  return a + b;
+}
+
+add(4,5);
+
+function subtract(a, b) {
+  return a - b;
+}
+
+add(30, 20);
+
+function multiply(a,b) {
+  return a * b;
+}
+
+multiply(8,4);
+
+function divide(a,b) {
+  return a / b;
+}
+
+divide(10, 2);
+
+//Math assignment
+
+function inc(n) {
+  return n += 1;
+}
+
+inc();
+
+function dec(n) {
+  return n -= 1;
+}
+
+dec();
+
+//Floats and Parse 
+
+function makeInt(n) {
+  return n.parseInt('2');
+}
+makeInt();
+
+
+
+
